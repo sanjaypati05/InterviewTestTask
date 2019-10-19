e8bc54416546b0751afe4efd3f95f45b826b435b
sanjaypati05 <sanjaypati05@gmail.com>
2019-10-19 14:21:27 +0530

    added initial code

---
+++ src/main/java/pageObject/challengingDOM/ChallengingDOM.java
@@ -0,0 +1,14 @@
+package pageObject.challengingDOM;
+
+import org.testng.annotations.Test;
+
+public class ChallengingDOM {
+
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("print ");
+		
+	}
+}
